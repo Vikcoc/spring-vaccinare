70df406831fdf849c9dc0151ad677e39fe71c720
Cociobanu Victor-Andrei <62379953+Vikcoc@users.noreply.github.com>
2021-05-04 17:51:01 +0300

    [Initial]

---
+++ src/main/java/com/vaccin/vaccin/VaccinApplication.java
@@ -0,0 +1,13 @@
+package com.vaccin.vaccin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaccin/vaccin/VaccinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaccin.vaccin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
