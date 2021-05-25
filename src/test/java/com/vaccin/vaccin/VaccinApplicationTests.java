4ede854455401e7226f3192af58a0426eb5f63f6
Cociobanu Victor-Andrei <62379953+Vikcoc@users.noreply.github.com>
2021-05-25 23:40:56 +0300

    [Poc] This is a type of test

--- src/test/java/com/vaccin/vaccin/VaccinApplicationTests.java
+++ src/test/java/com/vaccin/vaccin/VaccinApplicationTests.java
@@ -1,8 +1,10 @@
 package com.vaccin.vaccin;
 
 import com.vaccin.vaccin.model.VaccineCenter;
+import com.vaccin.vaccin.model.VaccineType;
 import com.vaccin.vaccin.repository.VaccineCenterRepository;
 import com.vaccin.vaccin.repository.VaccineTypeRepository;
+import com.vaccin.vaccin.service.VaccineCenterService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,19 +24,6 @@ import java.util.List;
 @RunWith(MockitoJUnitRunner.class)
 public class VaccinApplicationTests {
 
-
-////	@TestConfiguration
-////	static class EmployeeServiceImplTestContextConfiguration {
-////
-////		@Bean
-////		public VaccineCenterService vaccineCenterService() {
-////			return new VaccineCenterService();
-////		}
-////	}
-//
-//	@Autowired
-//	private VaccineCenterService vaccineCenterService;
-//
 	@Mock
 	private VaccineCenterRepository vaccineCenterRepository;
 	@Mock
@@ -42,35 +31,25 @@ public class VaccinApplicationTests {
 
 	@Before
 	public void setUp() {
-//		Mock.init(vaccineCenterRepository);
 		VaccineCenter vc = new VaccineCenter();
 		vc.setAddress("O adresa");
+		vc.setLongitude(0D);
+		vc.setLatitude(0D);
+		VaccineType vt = new VaccineType();
+		vt.setBrand("Some brand");
+		vc.setVaccineType(vt);
 		var lst = new ArrayList<>();
 		lst.add(vc);
 		Mockito.when(vaccineCenterRepository.findAll())
 				.thenReturn((List) lst);
 	}
 
-//	@Before
-//	public void runBare(){
-//		System.out.println("before");
-//	}
-//
-//	@Test
-//	public void testingMethod(){
-//		System.out.println("testing");
-//	}
-//
-//	@After
-//	public void setDirty(){
-//		System.out.println("after");
-//	}
-
 	@Test
 	public void contextLoads() {
-		System.out.println("Is in test");
-		System.out.println(vaccineCenterRepository.findAll().stream().findFirst().get().getAddress());
-		Assert.notEmpty(vaccineCenterRepository.findAll());
+		var service = new VaccineCenterService(vaccineCenterRepository, vaccineTypeRepository);
+//		System.out.println(service.getCentersAroundCoords(0D,0D));
+		System.out.println("This is to see that the test works");
+		Assert.notEmpty(service.getCentersAroundCoords(0D,0D), "It may work");
 	}
 
 }
