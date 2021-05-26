5aa0d3b044c3404a060b26b578d284b15ce09a03
Cociobanu Victor-Andrei <62379953+Vikcoc@users.noreply.github.com>
2021-05-26 21:58:52 +0300

    [Feature] five simple tests

---
+++ src/test/java/HelperForTesting/GenerateCenters.java
@@ -0,0 +1,42 @@
+package HelperForTesting;
+
+import com.vaccin.vaccin.model.VaccineCenter;
+import com.vaccin.vaccin.model.VaccineType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateCenters {
+
+    static public List<VaccineCenter> getCenters(Long startId, Integer count){
+        var res = new ArrayList();
+        for(int i = 1; i <= count; i++){
+            VaccineCenter vc = new VaccineCenter();
+            vc.setId(startId + i);
+            vc.setAddress("O adresa " + i);
+            vc.setLongitude(((double) i)/ 10);
+            vc.setLatitude(((double) i)/ 10);
+
+            VaccineType vt = new VaccineType();
+            vt.setBrand("Some brand " + i);
+            vc.setVaccineType(vt);
+
+            res.add(vc);
+        }
+        return res;
+    }
+
+    static public List<VaccineType> getTypes(Long startId, Integer count){
+        var res = new ArrayList();
+        for(int i = 1; i <= count; i++){
+
+            VaccineType vt = new VaccineType();
+            vt.setId(startId + i);
+            vt.setBrand("Some brand " + i);
+            vt.setDaysBetweenShots(60);
+
+            res.add(vt);
+        }
+        return res;
+    }
+}
--- src/test/java/com/vaccin/vaccin/VaccinApplicationTests.java
+++ src/test/java/com/vaccin/vaccin/VaccinApplicationTests.java
@@ -1,26 +1,24 @@
 package com.vaccin.vaccin;
 
-import com.vaccin.vaccin.model.VaccineCenter;
+import HelperForTesting.GenerateCenters;
+import com.vaccin.vaccin.dto.VaccineCenterCreateDto;
 import com.vaccin.vaccin.model.VaccineType;
 import com.vaccin.vaccin.repository.VaccineCenterRepository;
 import com.vaccin.vaccin.repository.VaccineTypeRepository;
 import com.vaccin.vaccin.service.VaccineCenterService;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.mockito.ArgumentMatchers;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.util.Assert;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import static org.mockito.Mockito.verify;
 
-//@RunWith(JUnit4.class)
 @RunWith(MockitoJUnitRunner.class)
 public class VaccinApplicationTests {
 
@@ -31,25 +29,78 @@ public class VaccinApplicationTests {
 
 	@Before
 	public void setUp() {
-		VaccineCenter vc = new VaccineCenter();
-		vc.setAddress("O adresa");
-		vc.setLongitude(0D);
-		vc.setLatitude(0D);
-		VaccineType vt = new VaccineType();
-		vt.setBrand("Some brand");
-		vc.setVaccineType(vt);
-		var lst = new ArrayList<>();
-		lst.add(vc);
 		Mockito.when(vaccineCenterRepository.findAll())
-				.thenReturn((List) lst);
+				.thenReturn((List) GenerateCenters.getCenters(0L,6));
+
+		Mockito.when(vaccineTypeRepository.findById(0L))
+				.thenReturn(Optional.empty());
+
+		VaccineType vt = new VaccineType();
+		vt.setId(1L);
+		vt.setBrand("Some brand 1");
+		vt.setDaysBetweenShots(60);
+		Mockito.when(vaccineTypeRepository.findById(1L))
+				.thenReturn(Optional.of(vt));
 	}
 
 	@Test
-	public void contextLoads() {
+	public void getCentersAroundCoords_GetsByRightCoords() {
 		var service = new VaccineCenterService(vaccineCenterRepository, vaccineTypeRepository);
-//		System.out.println(service.getCentersAroundCoords(0D,0D));
-		System.out.println("This is to see that the test works");
-		Assert.notEmpty(service.getCentersAroundCoords(0D,0D), "It may work");
+		assert !service.getCentersAroundCoords(0.1D,0.1D).isEmpty() : "List is empty";
+	}
+
+	@Test
+	public void getCentersAroundCoords_DoesntGetByWrong() {
+		var service = new VaccineCenterService(vaccineCenterRepository, vaccineTypeRepository);
+		assert service.getCentersAroundCoords(1D,1D).isEmpty() : "List is not empty";
+	}
+
+	@Test
+	public void addCenter_GivesAffirmativeAnswer(){
+		var service = new VaccineCenterService(vaccineCenterRepository, vaccineTypeRepository);
+		VaccineCenterCreateDto val = new VaccineCenterCreateDto();
+		val.setAddress("Some address");
+		val.setLatitude(0D);
+		val.setLongitude(0D);
+		val.setVaccineTypeId(1);
+		val.setName("Some type");
+		val.setDosesAvailable(400);
+
+		var res = service.addCenter(val);
+
+		assert res == "Vaccine Center added";
+	}
+
+	@Test
+	public void addCenter_GivesNegativeAnswer(){
+		var service = new VaccineCenterService(vaccineCenterRepository, vaccineTypeRepository);
+		VaccineCenterCreateDto val = new VaccineCenterCreateDto();
+		val.setAddress("Some address");
+		val.setLatitude(0D);
+		val.setLongitude(0D);
+		val.setVaccineTypeId(0);
+		val.setName("Some type");
+		val.setDosesAvailable(400);
+
+		var res = service.addCenter(val);
+
+		assert res == "Vaccine Type not found";
+	}
+
+	@Test
+	public void addCenter_SavesToRepo(){
+		var service = new VaccineCenterService(vaccineCenterRepository, vaccineTypeRepository);
+		VaccineCenterCreateDto val = new VaccineCenterCreateDto();
+		val.setAddress("Some address");
+		val.setLatitude(0D);
+		val.setLongitude(0D);
+		val.setVaccineTypeId(1);
+		val.setName("Some type");
+		val.setDosesAvailable(400);
+
+		service.addCenter(val);
+
+		verify(vaccineCenterRepository).save(ArgumentMatchers.any());
 	}
 
 }
