3a06b49a5609e831c2c1e7c8887e429513fdd595
Andrei Tudose <andreitudose2000@gmail.com>
2021-06-13 18:01:31 +0300

    Get User by Email

--- src/main/java/com/vaccin/vaccin/repository/UserRepository.java
+++ src/main/java/com/vaccin/vaccin/repository/UserRepository.java
@@ -19,6 +19,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Query("SELECT u.name FROM User u WHERE u.cnp = ?1")
     Optional<User> getByCnp(String cnp);
 
-    @Query("SELECT u.name FROM User u WHERE u.email = ?1")
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
     Optional<User> getByEmail(String email);
 }
